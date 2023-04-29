288668a2e8c1d6b017f40972ec31624f1de77f17
achen9149 <aaronchen9149@gmail.com>
2023-04-28 20:05:19 -0400

    create controller folder and login/signup method signup

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //routers for login
-router.get('/login', (req,res) => {
-  return res.status(200).json('we have successfully logged in')
-})
+router.get("/login", (req, res) => {
+  return res.status(200).json("we have successfully logged in");
+});
 //routers for signup
-router.post('/signup', (req,res) => {
-  return res.status(200).json('we have successfully signed up')
-})
+router.post("/signup", (req, res) => {
+  return res.status(200).json("we have successfully signed up");
+});
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 // RUNNING SERVER
 const app = express();
@@ -9,27 +9,28 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cors());
 
-app.use('/build', express.static(path.join(__dirname, '../build')));
+app.use("/build", express.static(path.join(__dirname, "../build")));
 
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
+app.get("/", (req, res) => {
+  return res
+    .status(200)
+    .sendFile(path.resolve(__dirname, "../client/index.html"));
 });
-//unknown route handler 
-app.get('*', (req,res) => {
-  res.status(404).json('Unknown route error')
-})
+//unknown route handler
+app.get("*", (req, res) => {
+  res.status(404).json("Unknown route error");
+});
 // GLOBAL ERROR ROUTE
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
+    log: "Express error handler caught unknown middleware error",
     status: 400,
-    message: { err: 'An error occurred' },
+    message: { err: "An error occurred" },
   };
   const errorObj = Object.assign({}, defaultErr, err);
   console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
 });
-
 
 // SERVER
 app.listen(PORT, function () {
