0e4a2c04232ac3747d98122584c3b1bee0dc2745
paulrvach <paulrvach@gmail.com>
2023-05-01 17:05:29 -0400

    working version

--- client/components/BotChatBubble.jsx
+++ client/components/BotChatBubble.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const BotChatBubble = ({ message }) => {
+  // console.log('AUDIO URL', message.audio)
   return (
     <div className='chat chat-start w-96 my-2'>
       <div className='chat-image avatar'>
@@ -11,7 +12,12 @@ const BotChatBubble = ({ message }) => {
       <div className='chat-header'>
         {message.createdBy}
         {/*  */}
-        <time className='text-xs opacity-50 ml-2'>{new Date(message.time).toLocaleTimeString('en-US')}</time>
+        <time className='text-xs opacity-50 ml-2'>
+          {new Date(message.time).toLocaleTimeString('en-US')}
+        </time>
+      </div>
+      <div>
+        <audio type='audio/mpeg' controls src={message.audio}></audio>
       </div>
       <div className='chat-bubble bg-secondary'>{message.text}</div>
       {/* <div className='chat-footer opacity-50'>Delivered</div> */}
